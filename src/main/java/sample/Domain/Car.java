227be92d9e1ab805378011fde8c3be616bbb22a6
marius3 <mariusmuresanu@yahoo.com>
2019-04-20 13:38:12 +0300

    Nicer json formatting.

--- src/main/java/sample/Domain/Car.java
+++ src/main/java/sample/Domain/Car.java
@@ -1,4 +1,48 @@
 package sample.Domain;
 
-public class Car {
+public class Car extends Entity{
+
+    private String model;
+    private int startKm, price;
+
+
+    public Car(String id, String model, int startKm, int price) {
+        super(id);
+        this.model = model;
+        this.startKm = startKm;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getStartKm() {
+        return startKm;
+    }
+
+    public void setStartKm(int startKm) {
+        this.startKm = startKm;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", startKm=" + startKm +
+                ", price=" + price +
+                '}';
+    }
 }
